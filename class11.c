320c841ba49ae2869c62bc30f729730be0e3de4f
edsonvsantos <edsonvsantos.235@gmail.com>
2024-10-25 12:25:11 -0300

    Add files via upload

---
+++ class11.c
@@ -0,0 +1,20 @@
+// Biblioteca / Library
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Tipos primitivos
+// int, float, char, bool
+
+// Variáveis de escopo global
+int numGlobal;
+
+// Função / Método principal
+int main (){
+	// Variáveis de escopo local
+	float numQuebrado;
+	
+	// Inicializadores de variáveis com valores
+	char letter = 'a';
+	bool ativo = false;
+}
